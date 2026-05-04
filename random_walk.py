adf1f4c3f809e7174aea5adb90626cf5e48d230b
blau28-create <blau28@saddleriverday.org>
2026-05-03 22:24:02 -0400

    new get_step() method

--- random_walk.py
+++ random_walk.py
@@ -9,20 +9,18 @@ class RandomWalk:
 
     def fill_walk(self):
         while len(self.x_values) < self.num_points:
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2 , 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
-
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             x = self.x_values[-1] + x_step
             y = self.y_values[-1] + y_step
 
             self.x_values.append(x)
             self.y_values.append(y)
+    def get_step(self):
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4])
+        return direction * distance
 
 random_walk = RandomWalk()
 random_walk.fill_walk()
